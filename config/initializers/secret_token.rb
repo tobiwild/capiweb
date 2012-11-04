3cff21750311345aebbc12ebf650fd0cd0412796
Tobias Wild <tobiwild@web.de>
2012-11-04 15:00:47 +0100

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapiwebRails::Application.config.secret_token = '842805e5c44b55dbdc681e342bd03334977c21d6475bc1993b4b1edc6cd62400984fd6c94c7f963eb438d549e9297fb5a871ee23faf9570f77db20ffaf578f61'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+CapiwebRails::Application.routes.draw do
+  root :to => "home#index"
+end
